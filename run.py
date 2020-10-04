9ed791cb64a6afa86d5c522cf025eb3f89ad4e13
EwetoyeIbrahim <i.ewetoye@gmail.com>
2020-10-04 07:06:50 +0000

    Requirements and Templates Fixes

--- run.py
+++ run.py
@@ -3,5 +3,5 @@ from equimolar_blog import create_app
 app = create_app(os.getenv('CONFIG_NAME', default='default'))
 if __name__ == '__main__':
     #print(app.url_map)
-    app.run(host='127.0.0.1', port=5000)
+    app.run(host='0.0.0.0', port=5000)
     
